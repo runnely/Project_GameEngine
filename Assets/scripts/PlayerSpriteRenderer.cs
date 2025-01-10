143e52d0c9edf438ac88b2f04f6a8a9a96fea27c
runnelly <arkabaevelnur@gmail.com>
2025-01-10 07:25:36 +0100

    run animation has been improved

--- Assets/scripts/PlayerSpriteRenderer.cs
+++ Assets/scripts/PlayerSpriteRenderer.cs
@@ -10,7 +10,7 @@ public class PlayerSpriteRenderer : MonoBehaviour
     public Sprite idle;
     public Sprite jump;
     public Sprite slide;
-    public Sprite run;
+    public AnimatedSprite run;
 
     private void Awake()
     {
@@ -18,8 +18,20 @@ public class PlayerSpriteRenderer : MonoBehaviour
         movement = GetComponentInParent<PlayerMovement>();
     }
 
+    private void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        spriteRenderer.enabled = false;
+    }
+
     private void LateUpdate()
     {
+        run.enabled = movement.running;
+
         if (movement.jumping)
         {
             spriteRenderer.sprite = jump;
@@ -28,11 +40,9 @@ public class PlayerSpriteRenderer : MonoBehaviour
         {
             spriteRenderer.sprite = slide;
         }
-        else if (movement.running)
-        {
-            spriteRenderer.sprite = run;
-        }
-        else { spriteRenderer.sprite = idle;
+        else if (!movement.running) 
+        { 
+            spriteRenderer.sprite = idle;
         }
     }
 }
