ef4efcd5aaa07e97f9a1a3d181d8188faa762cb1
runnelly <arkabaevelnur@gmail.com>
2025-01-10 05:32:02 +0100

    direction rotation has been added

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -49,6 +49,15 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.x = 0f;
         }
+
+        if (velocity.x > 0f)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (velocity.x < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
     }
 
     private void GroundedMovement()
