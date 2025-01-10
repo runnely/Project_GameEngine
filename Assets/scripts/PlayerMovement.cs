61671f3bbcea4c84e68781823ae36bb5b837be83
runnelly <arkabaevelnur@gmail.com>
2025-01-10 04:45:31 +0100

    player movement added and also head collision has been fixed

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -77,4 +77,15 @@ public class PlayerMovement : MonoBehaviour
         rigidbody.MovePosition(position);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        {
+            if (transform.DotTest(collision.transform, Vector2.up))
+            {
+                velocity.y = 0f;
+            }
+        }
+    }
+
 }
