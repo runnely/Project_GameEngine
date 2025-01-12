fa1cac065bf443bf28fba73efbcbad4934d9664e
runnelly <arkabaevelnur@gmail.com>
2025-01-12 06:56:22 +0100

    death animation added

--- Assets/scripts/DeathAnimation.cs
+++ Assets/scripts/DeathAnimation.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class DeathAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer spriteRenderer;
+    public Sprite deadSprite;
+
+    private void Reset()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
+        UpdateSprite();
+        DisabalePhysics();
+        StartCoroutine(Animate());
+    }
+
+    private void UpdateSprite()
+    {
+        spriteRenderer.enabled = true;
+        spriteRenderer.sortingOrder = 10;
+
+        if (deadSprite != null)
+        {
+            spriteRenderer.sprite = deadSprite;
+        }
+    }
+    private void DisabalePhysics()
+    {
+        Collider2D[] colliders = GetComponents<Collider2D>();
+
+        foreach (Collider2D collider in colliders)
+        {
+            collider.enabled = false;
+        }
+
+        PlayerMovement playerMovement = GetComponent<PlayerMovement>();
+        EntityMovement entityMovement = GetComponent<EntityMovement>();
         
+        if (playerMovement != null)
+        {
+            playerMovement.enabled = false;
+        }
+        if (entityMovement != null)
+        {
+            entityMovement.enabled = false;
+        }
+    }
+
+    private IEnumerator Animate()
+    {
+        float elapsed = 0f;
+        float duration = 3f;
+
+        float jumpVelocity = 10f;
+        float gravity = -36f;
+
+        Vector3 velocity = Vector3.up * jumpVelocity;
+
+        while (elapsed < duration)
+        {
+            transform.position += velocity * Time.deltaTime;
+            velocity.y += gravity * Time.deltaTime;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
     }
 }
